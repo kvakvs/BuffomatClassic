9ab49e734fa3efa6bd109b22faf7f49cba745a10
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2022-07-19 17:03:49 +0200

    Options split into smaller funcs

--- Src/Ui/Options.lua
+++ Src/Ui/Options.lua
@@ -146,106 +146,130 @@ function optionsModule:TemplateRange(name, rangeFrom, rangeTo, step, dict, key, 
   }
 end
 
+function optionsModule:CreateGeneralOptionsTable()
+  return {
+    type = "group",
+    name = "1. " .. _t("options.general.group.General"),
+    args = {
+      autoOpen              = self:TemplateCheckbox("AutoOpen"),
+      useProfiles           = self:TemplateCheckbox("UseProfiles", BOM.CharacterState),
+      slowerHardware        = self:TemplateCheckbox("SlowerHardware"),
+      minimapButtonShow     = self:TemplateCheckbox(
+              "ShowMinimapButton", BOM.SharedState.Minimap, "visible"),
+      minimapButtonLock     = self:TemplateCheckbox(
+              "LockMinimapButton", BOM.SharedState.Minimap, "lock"),
+      minimapButtonLockDist = self:TemplateCheckbox(
+              "LockMinimapButtonDistance", BOM.SharedState.Minimap, "lockDistance"),
+      uiWindowScale         = self:TemplateInput("float", "UIWindowScale"),
+    }
+  }
+end
+
+function optionsModule:CreateScanOptionsTable()
+  return {
+    type = "group",
+    name = "2. " .. _t("options.general.group.Scan"),
+    args = {
+      scanInRestArea   = self:TemplateCheckbox("ScanInRestArea"),
+      scanInStealth    = self:TemplateCheckbox("ScanInStealth"),
+      scanWhileMounted = self:TemplateCheckbox("ScanWhileMounted"),
+      inWorld          = self:TemplateCheckbox("InWorld"),
+      inPVP            = self:TemplateCheckbox("InPVP"),
+      inInstance       = self:TemplateCheckbox("InInstance"),
+      sameZone         = self:TemplateCheckbox("SameZone"),
+    }
+  }
+end
+
+function optionsModule:CreateAutoActionOptionsTable()
+  return {
+    type = "group",
+    name = "3. " .. _t("options.general.group.AutoActions"),
+    args = {
+      autoCrusaderAura   = self:TemplateCheckbox("AutoCrusaderAura"),
+      autoDismount       = self:TemplateCheckbox("AutoDismount"),
+      autoDismountFlying = self:TemplateCheckbox("AutoDismountFlying"),
+      autoStand          = self:TemplateCheckbox("AutoStand"),
+      autoDisTravel      = self:TemplateCheckbox("AutoDisTravel"),
+    }
+  }
+end
+
+function optionsModule:CreateConvenienceOptionsTable()
+  return {
+    type = "group",
+    name = "4. " .. _t("options.general.group.Convenience"),
+    args = {
+      preventPVPTag          = self:TemplateCheckbox("PreventPVPTag"),
+      deathBlock             = self:TemplateCheckbox("DeathBlock"),
+      noGroupBuff            = self:TemplateCheckbox("NoGroupBuff"),
+      resGhost               = self:TemplateCheckbox("ResGhost"),
+      replaceSingle          = self:TemplateCheckbox("ReplaceSingle"),
+      argentumDawn           = self:TemplateCheckbox("ArgentumDawn"),
+      carrot                 = self:TemplateCheckbox("Carrot"),
+      mainHand               = self:TemplateCheckbox("MainHand"),
+      secondaryHand          = self:TemplateCheckbox("SecondaryHand"),
+      useRank                = self:TemplateCheckbox("UseRank"),
+      buffTarget             = self:TemplateCheckbox("BuffTarget"),
+      openLootable           = self:TemplateCheckbox("OpenLootable"),
+      selfFirst              = self:TemplateCheckbox("SelfFirst"),
+      dontUseConsumables     = self:TemplateCheckbox("DontUseConsumables"),
+      hideSomeoneIsDrinking  = self:TemplateCheckbox("HideSomeoneIsDrinking"),
+      activateBomOnSpiritTap = self:TemplateRange("ActivateBomOnSpiritTap", 0, 100, 10),
+    },
+  }
+end
+
+function optionsModule:CreateBuffingOptionsTable()
+  return {
+    type = "group",
+    name = "5. " .. _t("options.general.group.Buffing"),
+    args = {
+      minBuff        = self:TemplateRange("MinBuff", 1, 5, 1),
+      minBlessing    = self:TemplateRange("MinBlessing", 1, 40, 1),
+      rebuffTime60   = self:TemplateRange("Time60", 10, 50, 5),
+      rebuffTime300  = self:TemplateRange("Time300", 30, 300 - 60, 10),
+      rebuffTime600  = self:TemplateRange("Time600", 30, 600 - 120, 10),
+      rebuffTime1800 = self:TemplateRange("Time1800", 30, 600 - 30, 30),
+      rebuffTime3600 = self:TemplateRange("Time3600", 30, 600 - 30, 30),
+    } -- end args
+  }
+end
+
+function optionsModule:CreateVisibilityOptionsTable()
+  return {
+    type = "group",
+    name = "6. " .. _t("options.general.group.Visibility"),
+    args = {
+      categories = self:TemplateMultiselect(
+              "VisibleCategories",
+              allSpellsModule:GetBuffCategories(), -- all categories ordered
+              BomCharacterState.BuffCategoriesHidden, -- settings table
+              nil,
+              function(state, key, value)
+                BomCharacterState.BuffCategoriesHidden[key] = not value -- invert
+                BOM.OnSpellsChanged()
+              end,
+              function(state, key)
+                return BomCharacterState.BuffCategoriesHidden[key] ~= true -- invert
+              end
+      ),
+    } -- end args
+  }
+end
+
 function optionsModule:CreateOptionsTable()
   self.optionsOrder = 0
 
   return {
     type = "group",
     args = {
-      generalOptions     = {
-        type = "group",
-        name = "1. ".._t("options.general.group.General"),
-        args = {
-          autoOpen              = self:TemplateCheckbox("AutoOpen"),
-          useProfiles           = self:TemplateCheckbox("UseProfiles", BOM.CharacterState),
-          slowerHardware        = self:TemplateCheckbox("SlowerHardware"),
-          minimapButtonShow     = self:TemplateCheckbox(
-                  "ShowMinimapButton", BOM.SharedState.Minimap, "visible"),
-          minimapButtonLock     = self:TemplateCheckbox(
-                  "LockMinimapButton", BOM.SharedState.Minimap, "lock"),
-          minimapButtonLockDist = self:TemplateCheckbox(
-                  "LockMinimapButtonDistance", BOM.SharedState.Minimap, "lockDistance"),
-          uiWindowScale         = self:TemplateInput("float", "UIWindowScale"),
-        }
-      },
-      scanOptions        = {
-        type = "group",
-        name = "2. ".._t("options.general.group.Scan"),
-        args = {
-          scanInRestArea   = self:TemplateCheckbox("ScanInRestArea"),
-          scanInStealth    = self:TemplateCheckbox("ScanInStealth"),
-          scanWhileMounted = self:TemplateCheckbox("ScanWhileMounted"),
-          inWorld          = self:TemplateCheckbox("InWorld"),
-          inPVP            = self:TemplateCheckbox("InPVP"),
-          inInstance       = self:TemplateCheckbox("InInstance"),
-          sameZone         = self:TemplateCheckbox("SameZone"),
-        }
-      },
-      autoActionOptions  = {
-        type = "group",
-        name = "3. ".._t("options.general.group.AutoActions"),
-        args = {
-          autoCrusaderAura   = self:TemplateCheckbox("AutoCrusaderAura"),
-          autoDismount       = self:TemplateCheckbox("AutoDismount"),
-          autoDismountFlying = self:TemplateCheckbox("AutoDismountFlying"),
-          autoStand          = self:TemplateCheckbox("AutoStand"),
-          autoDisTravel      = self:TemplateCheckbox("AutoDisTravel"),
-        }
-      },
-      convenienceOptions = {
-        type = "group",
-        name = "4. ".._t("options.general.group.Convenience"),
-        args = {
-          preventPVPTag          = self:TemplateCheckbox("PreventPVPTag"),
-          deathBlock             = self:TemplateCheckbox("DeathBlock"),
-          noGroupBuff            = self:TemplateCheckbox("NoGroupBuff"),
-          resGhost               = self:TemplateCheckbox("ResGhost"),
-          replaceSingle          = self:TemplateCheckbox("ReplaceSingle"),
-          argentumDawn           = self:TemplateCheckbox("ArgentumDawn"),
-          carrot                 = self:TemplateCheckbox("Carrot"),
-          mainHand               = self:TemplateCheckbox("MainHand"),
-          secondaryHand          = self:TemplateCheckbox("SecondaryHand"),
-          useRank                = self:TemplateCheckbox("UseRank"),
-          buffTarget             = self:TemplateCheckbox("BuffTarget"),
-          openLootable           = self:TemplateCheckbox("OpenLootable"),
-          selfFirst              = self:TemplateCheckbox("SelfFirst"),
-          dontUseConsumables     = self:TemplateCheckbox("DontUseConsumables"),
-          hideSomeoneIsDrinking  = self:TemplateCheckbox("HideSomeoneIsDrinking"),
-          activateBomOnSpiritTap = self:TemplateRange("ActivateBomOnSpiritTap", 0, 100, 10),
-        },
-      }, -- end convenience options
-      buffingOptions     = {
-        type = "group",
-        name = "5. ".._t("options.general.group.Buffing"),
-        args = {
-          minBuff        = self:TemplateRange("MinBuff", 1, 5, 1),
-          minBlessing    = self:TemplateRange("MinBlessing", 1, 40, 1),
-          rebuffTime60   = self:TemplateRange("Time60", 10, 50, 5),
-          rebuffTime300  = self:TemplateRange("Time300", 30, 300 - 60, 10),
-          rebuffTime600  = self:TemplateRange("Time600", 30, 600 - 120, 10),
-          rebuffTime1800 = self:TemplateRange("Time1800", 30, 600 - 30, 30),
-          rebuffTime3600 = self:TemplateRange("Time3600", 30, 600 - 30, 30),
-        } -- end args
-      }, -- end buffing options
-      visibilityOptions  = {
-        type = "group",
-        name = "6. ".._t("options.general.group.Visibility"),
-        args = {
-          categories = self:TemplateMultiselect(
-                  "VisibleCategories",
-                  allSpellsModule:GetBuffCategories(), -- all categories ordered
-                  BomCharacterState.BuffCategoriesHidden, -- settings table
-                  nil,
-                  function(state, key, value)
-                    BomCharacterState.BuffCategoriesHidden[key] = not value -- invert
-                    BOM.OnSpellsChanged()
-                  end,
-                  function(state, key)
-                    return BomCharacterState.BuffCategoriesHidden[key] ~= true -- invert
-                  end
-          ),
-        } -- end args
-      }, -- end buffing options
+      generalOptions     = self:CreateGeneralOptionsTable(),
+      scanOptions        = self:CreateScanOptionsTable(),
+      autoActionOptions  = self:CreateAutoActionOptionsTable(),
+      convenienceOptions = self:CreateConvenienceOptionsTable(),
+      buffingOptions     = self:CreateBuffingOptionsTable(),
+      visibilityOptions  = self:CreateVisibilityOptionsTable(),
     } -- end args
   } -- end
 end
