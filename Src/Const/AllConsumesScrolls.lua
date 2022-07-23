f347e4738c1a687fbefc21deaa1616d3e3087569
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2022-07-23 23:40:11 +0200

    Scrolls updated for WotLK

--- Src/Const/AllConsumesScrolls.lua
+++ Src/Const/AllConsumesScrolls.lua
@@ -11,55 +11,140 @@ local buffDefModule = BuffomatModule.Import("BuffDefinition") ---@type BomBuffDe
 ---@param buffs table<string, BomBuffDefinition> A list of buffs (not dictionary)
 ---@param enchantments table<number, table<number>> Key is spell id, value is list of enchantment ids
 function scrollsModule:SetupScrolls(buffs, enchantments)
+  self:_SetupScrollsAgility(buffs, enchantments)
+  self:_SetupScrollsStrength(buffs, enchantments)
+  self:_SetupScrollsProtection(buffs, enchantments)
+  self:_SetupScrollsSpirit(buffs, enchantments)
+  self:_SetupInscriptionScrolls(buffs, enchantments)
+end
+
+function scrollsModule:AddScroll(buffs, buffSpellId, itemId)
+  return buffDefModule:createAndRegisterBuff(buffs, buffSpellId,
+          { item           = itemId, isConsumable = true, default = false, consumableTarget = "player",
+            singleDuration = allBuffsModule.DURATION_30M,
+          })          :Category(allBuffsModule.SCROLL)
+end
+
+---@param buffs table<string, BomBuffDefinition> A list of buffs (not dictionary)
+---@param enchantments table<number, table<number>> Key is spell id, value is list of enchantment ids
+function scrollsModule:_SetupScrollsAgility(buffs, enchantments)
   --
-  -- Scrolls
+  -- WotLK
   --
-  buffDefModule:createAndRegisterBuff(buffs, 33077, --TBC: Scroll of Agility V
-          { item           = 27498, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_PHYSICAL_CLASSES })
-                :RequireTBC()
-                :Category(allBuffsModule.SCROLL)
-  buffDefModule:createAndRegisterBuff(buffs, 12174, --Scroll of Agility IV
-          { item           = 10309, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_PHYSICAL_CLASSES, })
-                :Category(allBuffsModule.SCROLL)
-  buffDefModule:createAndRegisterBuff(buffs, 8117, --Scroll of Agility III
-          { item           = 4425, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_PHYSICAL_CLASSES, })
-                :Category(allBuffsModule.SCROLL)
+  self:AddScroll(buffs, 58451, 43464) -- WotLK: Scroll of Agility 8
+      :ClassOnly(allBuffsModule.BOM_PHYSICAL_CLASSES)
+      :RequireWotLK()
+  self:AddScroll(buffs, 58450, 43463) -- WotLK: Scroll of Agility 7
+      :ClassOnly(allBuffsModule.BOM_PHYSICAL_CLASSES)
+      :RequireWotLK()
+  self:AddScroll(buffs, 43194, 33457) -- WotLK: Scroll of Agility 6
+      :ClassOnly(allBuffsModule.BOM_PHYSICAL_CLASSES)
+      :RequireWotLK()
 
-  buffDefModule:createAndRegisterBuff(buffs, 33082, --TBC: Scroll of Strength V
-          { item           = 27503, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_MELEE_CLASSES })
-                :RequireTBC()
-                :Category(allBuffsModule.SCROLL)
-  buffDefModule:createAndRegisterBuff(buffs, 12179, --Scroll of Strength IV
-          { item           = 10310, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_MELEE_CLASSES, })
-                :Category(allBuffsModule.SCROLL)
-  buffDefModule:createAndRegisterBuff(buffs, 8120, --Scroll of Strength III
-          { item           = 4426, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_MELEE_CLASSES, })
-                :Category(allBuffsModule.SCROLL)
+  --
+  -- TBC
+  --
+  self:AddScroll(buffs, 33077, 27498) -- TBC: Scroll of Agility 5
+      :ClassOnly(allBuffsModule.BOM_PHYSICAL_CLASSES)
+      :RequireTBC()
 
-  buffDefModule:createAndRegisterBuff(buffs, 33079, --TBC: Scroll of Protection V
-          { item           = 27500, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M })
-                :RequireTBC()
-                :Category(allBuffsModule.SCROLL)
-  buffDefModule:createAndRegisterBuff(buffs, 12175, --Scroll of Protection IV
-          { item           = 10305, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, })
-                :Category(allBuffsModule.SCROLL)
+  --
+  -- Classic
+  --
+  self:AddScroll(buffs, 12174, 10309) -- Scroll of Agility 4
+      :ClassOnly(allBuffsModule.BOM_PHYSICAL_CLASSES)
+  self:AddScroll(buffs, 8117, 4425) -- Scroll of Agility 3
+      :ClassOnly(allBuffsModule.BOM_PHYSICAL_CLASSES)
+end
 
-  buffDefModule:createAndRegisterBuff(buffs, 12177, --Scroll of Spirit IV
-          { item           = 10306, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_MANA_CLASSES })
-                :Category(allBuffsModule.SCROLL)
-  buffDefModule:createAndRegisterBuff(buffs, 33080, --Scroll of Spirit V
-          { item           = 27501, isConsumable = true, default = false, consumableTarget = "player",
-            singleDuration = allBuffsModule.DURATION_30M, playerClass = allBuffsModule.BOM_MANA_CLASSES })
-                :RequireTBC()
-                :Category(allBuffsModule.SCROLL)
+---@param buffs table<string, BomBuffDefinition> A list of buffs (not dictionary)
+---@param enchantments table<number, table<number>> Key is spell id, value is list of enchantment ids
+function scrollsModule:_SetupScrollsStrength(buffs, enchantments)
+  --
+  -- WotLK
+  --
+  self:AddScroll(buffs, 58449, 43466) -- WotLK: Scroll of Strength 8
+      :ClassOnly(allBuffsModule.BOM_MELEE_CLASSES)
+      :RequireWotLK()
+  self:AddScroll(buffs, 58448, 43465) -- WotLK: Scroll of Strength 7
+      :ClassOnly(allBuffsModule.BOM_MELEE_CLASSES)
+      :RequireWotLK()
+  self:AddScroll(buffs, 43199, 33462) -- WotLK: Scroll of Strength 6
+      :ClassOnly(allBuffsModule.BOM_MELEE_CLASSES)
+      :RequireWotLK()
 
+  --
+  -- TBC
+  --
+  self:AddScroll(buffs, 33082, 27503) -- TBC: Scroll of Strength 5
+      :ClassOnly(allBuffsModule.BOM_MELEE_CLASSES)
+      :RequireTBC()
+
+  --
+  -- Classic
+  --
+  self:AddScroll(buffs, 12179, 10310) -- Scroll of Strength 4
+      :ClassOnly(allBuffsModule.BOM_MELEE_CLASSES)
+  self:AddScroll(buffs, 8120, 4426) -- Scroll of Strength 3
+      :ClassOnly(allBuffsModule.BOM_MELEE_CLASSES)
+end
+
+---@param buffs table<string, BomBuffDefinition> A list of buffs (not dictionary)
+---@param enchantments table<number, table<number>> Key is spell id, value is list of enchantment ids
+function scrollsModule:_SetupScrollsProtection(buffs, enchantments)
+  --
+  -- WotLK
+  --
+  self:AddScroll(buffs, 58453, 43468) -- WotLK: Scroll of Protection 8
+      :RequireWotLK()
+  self:AddScroll(buffs, 58452, 43467) -- WotLK: Scroll of Protection 7
+      :RequireWotLK()
+  self:AddScroll(buffs, 43196, 33459) -- WotLK: Scroll of Protection 6
+      :RequireWotLK()
+
+  --
+  -- TBC
+  --
+  self:AddScroll(buffs, 33079, 27500) -- TBC: Scroll of Protection 5
+      :RequireTBC()
+  --
+  -- Classic
+  --
+  self:AddScroll(buffs, 12175, 10305) -- Scroll of Protection 4
+end
+
+---@param buffs table<string, BomBuffDefinition> A list of buffs (not dictionary)
+---@param enchantments table<number, table<number>> Key is spell id, value is list of enchantment ids
+function scrollsModule:_SetupScrollsSpirit(buffs, enchantments)
+  --
+  -- WotLK
+  --
+  self:AddScroll(buffs, 48104, 37098) -- WotLK: Scroll of Spirit 8
+      :ClassOnly(allBuffsModule.BOM_MANA_CLASSES)
+      :RequireWotLK()
+  self:AddScroll(buffs, 48103, 37097) -- WotLK: Scroll of Spirit 7
+      :ClassOnly(allBuffsModule.BOM_MANA_CLASSES)
+      :RequireWotLK()
+  self:AddScroll(buffs, 43197, 33460) -- WotLK: Scroll of Spirit 6
+      :ClassOnly(allBuffsModule.BOM_MANA_CLASSES)
+      :RequireWotLK()
+
+  --
+  -- TBC
+  --
+  self:AddScroll(buffs, 33080, 27501) -- TBC: Scroll of Spirit 5
+      :ClassOnly(allBuffsModule.BOM_MANA_CLASSES)
+      :RequireTBC()
+  --
+  -- Classic
+  --
+  self:AddScroll(buffs, 12177, 10306) -- Scroll of Spirit 4
+      :ClassOnly(allBuffsModule.BOM_MANA_CLASSES)
+end
+
+---@param buffs table<string, BomBuffDefinition> A list of buffs (not dictionary)
+---@param enchantments table<number, table<number>> Key is spell id, value is list of enchantment ids
+function scrollsModule:_SetupInscriptionScrolls(buffs, enchantments)
+  -- TODO: Brilliance scroll
+  -- TODO: Fortitude scroll
 end
